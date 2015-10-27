cabfd752abe4396aa3d4656d0f5cb1446ac1b841
Steve Lasker <StevenLasker@Hotmail.com>
2015-10-27 09:57:30 -0700

    Add Hello World Samples for Node & GO

---
+++ DockerDemos/Go_WebServerHelloWorld/src/WebServerHello.go
@@ -0,0 +1,16 @@
+package main
+ 
+import(
+	"fmt"
+	"net/http"
+	"runtime"
+)
+ 
+func indexHandler( w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello World, I'm running on %s with an %s CPU ", runtime.GOOS, runtime.GOARCH)
+}
+ 
+func main(){
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
+}
